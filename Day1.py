dc4e6dd3823ea0faf16b750eafcb1afc01c9266d
sachinjose <sachinjose16@gmail.com>
2021-02-15 10:26:43 +0530

    Day 1

---
+++ Day1.py
@@ -0,0 +1,19 @@
+print('Hello, I am the Doctor')
+print("Basically,('Run')")
+
+print("Right, this is", input(" I'm sorry, what's your name again? ") ," my companion")
+
+print(len(input("Tell me a word and I'll tell you how big it is: ")))
+
+variable = input('take input')
+
+a = 5
+b = 8
+
+print(a,b)
+
+a,b = b,a
+
+print(a,b)
+
+print(input("What is the name of the city you grew up in: ") + input("What was the name of your pet: "))
