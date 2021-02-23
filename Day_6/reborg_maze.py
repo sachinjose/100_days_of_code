cb6dd4077cb32df7d4034c0b6a95ba4950327e98
sachinjose <sachinjose16@gmail.com>
2021-02-23 11:55:43 +0530

    Day_6

--- Day_6/reborg_maze.py
+++ Day_6/reborg_maze.py
@@ -19,3 +19,25 @@ while not at_goal():
 	else:
 		turn_left()
 		move()
+
+def turn_right():
+	turn_left()
+	turn_left()
+	turn_left()
+
+while front_is_clear():
+	move()
+turn_left()
+
+while not at_goal():
+	if right_is_clear():
+		turn_right()
+		move()
+
+	elif front_is_clear():
+		while front_is_clear():
+			move()
+            
+	else:
+		turn_left()
+		move()
